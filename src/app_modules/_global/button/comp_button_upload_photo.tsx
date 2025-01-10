60a8435fbae8f20fe5b0cf13251ef393e40a66a9
Bagasbanuna02 <bagasbanuna02@gmail.com>
2025-01-10 16:46:48 +0800

    fix ( upload ) deskripsi: - upload pada profile tidak dibatasi kapasitasnya hanya saat akan dikirim akan di resize

--- src/app_modules/_global/button/comp_button_upload_photo.tsx
+++ src/app_modules/_global/button/comp_button_upload_photo.tsx
@@ -1,12 +1,10 @@
 "use client";
 
+import { clientLogger } from "@/util/clientLogger";
 import { Button, FileButton } from "@mantine/core";
 import { IconCamera } from "@tabler/icons-react";
+import { useState } from "react";
 import { MainColor } from "../color";
-import { MAX_SIZE } from "../lib";
-import { PemberitahuanMaksimalFile } from "../lib/max_size";
-import { ComponentGlobal_NotifikasiPeringatan } from "../notif_global";
-import { clientLogger } from "@/util/clientLogger";
 
 export function ComponentGlobal_ButtonUploadFileImage({
   onSetFile,
@@ -15,22 +13,27 @@ export function ComponentGlobal_ButtonUploadFileImage({
   onSetFile: File | any;
   onSetImage: any | null;
 }) {
+  const [isLoading, setIsLoading] = useState(false);
+
   return (
     <FileButton
       onChange={async (files: any | null) => {
         try {
+          setIsLoading(true);
           const buffer = URL.createObjectURL(
             new Blob([new Uint8Array(await files.arrayBuffer())])
           );
 
-          if (files.size > MAX_SIZE) {
-            ComponentGlobal_NotifikasiPeringatan(PemberitahuanMaksimalFile);
-            return;
-          } else {
-            onSetFile(files);
-            onSetImage(buffer);
-          }
+          // if (files.size > MAX_SIZE) {
+          //   ComponentGlobal_NotifikasiPeringatan(PemberitahuanMaksimalFile);
+          //   return;
+          // } else {
+          // }
+          onSetFile(files);
+          onSetImage(buffer);
+          setIsLoading(false);
         } catch (error) {
+          setIsLoading(false);
           clientLogger.error("Upload error:", error);
         }
       }}
@@ -39,6 +42,8 @@ export function ComponentGlobal_ButtonUploadFileImage({
       {(props) => (
         <Button
           {...props}
+          loading={isLoading}
+          loaderPosition="center"
           radius={"xl"}
           leftIcon={<IconCamera />}
           bg={MainColor.yellow}
