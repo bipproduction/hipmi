4d8e39c318e58948f179a7f214926dc5ff6f8c51
Bagasbanuna02 <bagasbanuna02@gmail.com>
2025-02-12 17:32:42 +0800

    fix api edit logo

--- src/app_modules/katalog/portofolio/component/skeleton_view.tsx
+++ src/app_modules/katalog/portofolio/component/skeleton_view.tsx
@@ -1,7 +1,7 @@
 import CustomSkeleton from "@/app_modules/components/CustomSkeleton";
 import { Stack } from "@mantine/core";
 
-export { Portofolio_SkeletonEditDataBisnis };
+export { Portofolio_SkeletonEditDataBisnis, Portofolio_SkeletonEditLogoBisnis };
 
 function Portofolio_SkeletonEditDataBisnis() {
   return (
@@ -12,6 +12,16 @@ function Portofolio_SkeletonEditDataBisnis() {
         ))}
         <CustomSkeleton height={100} width={"100%"} />
         <CustomSkeleton radius="xl" height={50} width={"100%"} />
+      </Stack>
+    </>
+  );
+}
+
+function Portofolio_SkeletonEditLogoBisnis() {
+  return (
+    <>
+      <Stack spacing={"xl"} px={"sm"}>
+        <CustomSkeleton height={250} width={"100%"} />
       </Stack>
     </>
   );
