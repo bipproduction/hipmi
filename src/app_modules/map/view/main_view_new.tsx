829c211e87ccb8bc876bc2828c6eb1a6e2f073f6
Bagasbanuna02 <bagasbanuna02@gmail.com>
2025-03-13 15:36:03 +0800

    fix map layout

--- src/app_modules/map/view/main_view_new.tsx
+++ src/app_modules/map/view/main_view_new.tsx
@@ -1,13 +1,25 @@
-import UIGlobal_LayoutTamplate from "@/app_modules/_global/ui/ui_layout_tamplate";
-import { ComponentMap_Header } from "../_component";
+import { Component_Header } from "@/app_modules/_global/component/new/component_header";
+import UI_NewLayoutTamplate, {
+  UI_NewChildren,
+  UI_NewHeader,
+} from "@/app_modules/_global/ui/V2_layout_tamplate";
 import { UiMap_MapBoxViewNew } from "../ui/ui_map_new";
 
 export async function Map_ViewNew({ mapboxToken }: { mapboxToken: string }) {
   return (
     <>
-      <UIGlobal_LayoutTamplate header={<ComponentMap_Header />}>
+      {/* <UIGlobal_LayoutTamplate header={<ComponentMap_Header />}>
         <UiMap_MapBoxViewNew mapboxToken={mapboxToken} />
-      </UIGlobal_LayoutTamplate>
+      </UIGlobal_LayoutTamplate> */}
+
+      <UI_NewLayoutTamplate>
+        <UI_NewHeader>
+          <Component_Header title="Business Maps" />
+        </UI_NewHeader>
+        <UI_NewChildren>
+          <UiMap_MapBoxViewNew mapboxToken={mapboxToken} />
+        </UI_NewChildren>
+      </UI_NewLayoutTamplate>
     </>
   );
 }
