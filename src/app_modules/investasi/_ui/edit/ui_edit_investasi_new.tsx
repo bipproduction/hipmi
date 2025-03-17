e37bd163055609f79aaba0b146788a7a45f0383a
Bagasbanuna02 <bagasbanuna02@gmail.com>
2025-03-17 14:54:26 +0800

    fix layout investasi deskripsi: - fix detail portofolio - fix detail saham saya - fix detail transaksi

--- src/app_modules/investasi/_ui/edit/ui_edit_investasi_new.tsx
+++ src/app_modules/investasi/_ui/edit/ui_edit_investasi_new.tsx
@@ -1,16 +1,33 @@
 "use client";
 
-import { UIGlobal_LayoutHeaderTamplate, UIGlobal_LayoutTamplate, } from "@/app_modules/_global/ui";
+import {
+  UIGlobal_LayoutHeaderTamplate,
+  UIGlobal_LayoutTamplate,
+} from "@/app_modules/_global/ui";
 import { Investasi_ViewEditInvestasiNew } from "../../_view/edit/view_edit_investasi_new";
+import { Component_Header } from "@/app_modules/_global/component/new/component_header";
+import UI_NewLayoutTamplate, {
+  UI_NewHeader,
+  UI_NewChildren,
+} from "@/app_modules/_global/ui/V2_layout_tamplate";
 
 export function Investasi_UiEditInvestasiNew() {
-   return (
-      <>
-         <UIGlobal_LayoutTamplate
-            header={<UIGlobal_LayoutHeaderTamplate title="Edit Investasi" />}
-         >
-            <Investasi_ViewEditInvestasiNew />
-         </UIGlobal_LayoutTamplate>
-      </>
-   );
+  return (
+    <>
+      {/* <UIGlobal_LayoutTamplate
+         header={<UIGlobal_LayoutHeaderTamplate title="Edit Investasi" />}
+       >
+         <Investasi_ViewEditInvestasiNew />
+       </UIGlobal_LayoutTamplate> */}
+
+      <UI_NewLayoutTamplate>
+        <UI_NewHeader>
+          <Component_Header title="Edit Investasi" />
+        </UI_NewHeader>
+        <UI_NewChildren>
+          <Investasi_ViewEditInvestasiNew />
+        </UI_NewChildren>
+      </UI_NewLayoutTamplate>
+    </>
+  );
 }
