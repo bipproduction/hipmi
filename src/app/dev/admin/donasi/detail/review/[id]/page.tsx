cdc2664f6a4090fc023065267a2ef39210cb3ed0
Bagasbanuna02 <bagasbanuna02@gmail.com>
2025-05-02 17:45:39 +0800

    fix admin - detail donasi dengan view khusus html

--- src/app/dev/admin/donasi/detail/review/[id]/page.tsx
+++ src/app/dev/admin/donasi/detail/review/[id]/page.tsx
@@ -1,8 +1,5 @@
 import { AdminDonasi_DetailReview } from "@/app_modules/admin/donasi";
-import { AdminDonasi_getOneById } from "@/app_modules/admin/donasi/fun/get/get_one_by_id";
 
 export default async function Page() {
-  
-
   return <AdminDonasi_DetailReview />;
 }
