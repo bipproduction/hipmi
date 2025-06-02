af903c0defc50b7d66d7611bf90f95018996a20a
Bagasbanuna02 <bagasbanuna02@gmail.com>
2025-06-02 16:15:31 +0800

    fix voting deskripsi: - ubah metode pemanggilan data dari SSR menjadi metode API , pada page: detail all
    No Issue

--- src/app/dev/(user)/vote/detail/publish/[id]/layout.tsx
+++ src/app/dev/(user)/vote/detail/publish/[id]/layout.tsx
@@ -1,29 +1,14 @@
-import { funGetUserIdByToken } from "@/app_modules/_global/fun/get";
 import { LayoutVote_DetailPublish } from "@/app_modules/vote";
-import { voting_funGetOneVotingbyId } from "@/app_modules/vote/fun/get/fun_get_one_by_id";
 import React from "react";
 
 export default async function Layout({
   children,
-  params,
 }: {
   children: React.ReactNode;
-  params: { id: string };
 }) {
-  const votingId = params.id;
-  const userLoginId = await funGetUserIdByToken();
-
-  const dataVoting = await voting_funGetOneVotingbyId(votingId);
-
   return (
     <>
-      <LayoutVote_DetailPublish
-        votingId={votingId}
-        userLoginId={userLoginId as string}
-        dataVoting={dataVoting}
-      >
-        {children}
-      </LayoutVote_DetailPublish>
+      <LayoutVote_DetailPublish>{children}</LayoutVote_DetailPublish>
     </>
   );
 }
--- src/app/dev/(user)/vote/detail/publish/[id]/page.tsx
+++ src/app/dev/(user)/vote/detail/publish/[id]/page.tsx
@@ -1,14 +1,9 @@
 import { Vote_DetailPublish } from "@/app_modules/vote";
-import { Vote_getListKontributorById } from "@/app_modules/vote/fun/get/get_list_kontributor_by_id";
-import { voting_funGetOneVotingbyId } from "@/app_modules/vote/fun/get/fun_get_one_by_id";
 
-export default async function Page({ params }: { params: { id: string } }) {
-  let voteId = params.id;
-  const dataVote = await voting_funGetOneVotingbyId(voteId);
-
+export default async function Page() {
   return (
     <>
-      <Vote_DetailPublish dataVote={dataVote as any} />
+      <Vote_DetailPublish />
     </>
   );
 }
