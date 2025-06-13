efaca622c28796d7b2ab1ff3039bf5d2fd42b482
Bagasbanuna02 <bagasbanuna02@gmail.com>
2025-06-13 15:09:14 +0800

    fix : version 1.4.26

--- src/app/dev/admin/developer/page.tsx
+++ src/app/dev/admin/developer/page.tsx
@@ -1,7 +1,4 @@
 import AdminDeveloper from "@/app_modules/admin/developer";
-import adminDeveloper_funGetListAllAdmin from "@/app_modules/admin/developer/fun/get/fun_get_list_all_admin";
-import adminDeveloper_funGetListAllUser from "@/app_modules/admin/developer/fun/get/fun_get_list_all_user";
-import _ from "lodash";
 
 export default async function Page() {
   // const listUser = await adminDeveloper_funGetListAllUser({ page: 1 });
