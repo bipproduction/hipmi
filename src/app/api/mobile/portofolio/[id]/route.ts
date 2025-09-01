41bf7ce45c9eb1e6ffccb58bb503f5826cb40d9b
Bagasbanuna02 <bagasbanuna02@gmail.com>
2025-09-01 17:14:36 +0800

    Portofolio Fix: - api/mobile/portofolio/[id]/route.ts: tambah put dan delete
    File:
    Add:
    - src/app/api/mobile/file/: upload & delete
    
    ### No Issue

--- src/app/api/mobile/portofolio/[id]/route.ts
+++ src/app/api/mobile/portofolio/[id]/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/lib";
 
-export { GET };
+export { GET, DELETE, PUT };
 
 async function GET(request: Request, { params }: { params: { id: string } }) {
   try {
@@ -44,6 +44,196 @@ async function GET(request: Request, { params }: { params: { id: string } }) {
       {
         success: false,
         message: "API Error Get Data Potofolio",
+        reason: (error as Error).message,
+      },
+      { status: 500 }
+    );
+  }
+}
+
+async function DELETE(request: Request, context: { params: { id: string } }) {
+  try {
+    const { id } = context.params;
+
+    const data = await prisma.portofolio.findUnique({
+      where: {
+        id: id,
+      },
+      include: {
+        BusinessMaps: {
+          select: {
+            pinId: true,
+            imageId: true,
+          },
+        },
+      },
+    });
+
+    try {
+      if (data?.logoId != null) {
+        const id = data?.logoId;
+        const deleteLogo = await fetch(
+          `https://wibu-storage.wibudev.com/api/files/${id}/delete`,
+          {
+            method: "DELETE",
+            headers: {
+              Authorization: `Bearer ${process.env.WS_APIKEY}`,
+            },
+          }
+        );
+
+        if (deleteLogo.ok) {
+          console.log("Success delete logo");
+        }
+      }
+
+      if (data?.BusinessMaps?.pinId != null) {
+        const pinId = data?.BusinessMaps?.pinId;
+        const deletePin = await fetch(
+          `https://wibu-storage.wibudev.com/api/files/${pinId}/delete`,
+          {
+            method: "DELETE",
+            headers: {
+              Authorization: `Bearer ${process.env.WS_APIKEY}`,
+            },
+          }
+        );
+
+        if (deletePin.ok) {
+          console.log(`Success delete pin`);
+        }
+
+        const imageId = data?.BusinessMaps?.imageId;
+        const deleteImage = await fetch(
+          `https://wibu-storage.wibudev.com/api/files/${imageId}/delete`,
+          {
+            method: "DELETE",
+            headers: {
+              Authorization: `Bearer ${process.env.WS_APIKEY}`,
+            },
+          }
+        );
+
+        if (deleteImage.ok) {
+          console.log(`Success delete image`);
+        }
+      }
+    } catch (error) {
+      console.error("Error delete logo", error);
+    }
+
+    const deletePortoMedsos = await prisma.portofolio_MediaSosial.delete({
+      where: {
+        portofolioId: id,
+      },
+    });
+
+    // const deleteMap = await prisma.businessMaps.delete({
+    //   where: {
+    //     portofolioId: id,
+    //   },
+    // });
+
+    // console.log("DELETE PORTOFOLIO MEDSOS >>", deletePortoMedsos);
+
+    const deletePortofolio = await prisma.portofolio.delete({
+      where: {
+        id: id,
+      },
+    });
+
+    return NextResponse.json(
+      { success: true, message: "Berhasil menghapus data" },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      {
+        success: false,
+        message: "Gagal menghapus data, coba lagi nanti (error: 500)",
+        reason: (error as Error).message,
+      },
+      { status: 500 }
+    );
+  }
+}
+
+async function PUT(request: Request, { params }: { params: { id: string } }) {
+  try {
+    const { id } = params;
+    const { data } = await request.json();
+    const { searchParams } = new URL(request.url);
+    const category = searchParams.get("category");
+
+    let message;
+
+    if (category === "detail") {
+      const updateDetail = await prisma.portofolio.update({
+        where: { id },
+        data: {
+          namaBisnis: data.namaBisnis,
+          alamatKantor: data.alamatKantor,
+          tlpn: data.tlpn,
+          deskripsi: data.deskripsi,
+          masterBidangBisnisId: data.masterBidangBisnisId,
+        },
+      });
+      if (!updateDetail) {
+        return NextResponse.json({
+          success: false,
+          message: "Gagal mengupdate detail portofolio",
+        });
+      }
+      message = "Berhasil mengupdate detail portofolio";
+    } else if (category === "medsos") {
+      const updateMedsos = await prisma.portofolio_MediaSosial.update({
+        where: { portofolioId: id },
+        data: {
+          facebook: data.facebook,
+          instagram: data.instagram,
+          tiktok: data.tiktok,
+          twitter: data.twitter,
+          youtube: data.youtube,
+        },
+      });
+
+      if (!updateMedsos) {
+        return NextResponse.json({
+          success: false,
+          message: "Gagal mengupdate medsos portofolio",
+        });
+      }
+      message = "Berhasil mengupdate medsos portofolio";
+    } else if (category === "logo") {
+      const updateLogo = await prisma.portofolio.update({
+        where: { id },
+        data: {
+          logoId: data.fileId,
+        },
+      });
+      if (!updateLogo) {
+        return NextResponse.json({
+          success: false,
+          message: "Gagal mengupdate logo portofolio",
+        });
+      }
+      message = "Berhasil mengupdate logo portofolio";
+    }
+
+    return NextResponse.json(
+      {
+        success: true,
+        message: message,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error("Error update data portofolio", error);
+    return NextResponse.json(
+      {
+        success: false,
+        message: "Gagal mengupdate data, coba lagi nanti (error: 500)",
         reason: (error as Error).message,
       },
       { status: 500 }
