a72cf866fa19fb8243ffedd18eda92b6d8176baa
bagasbanuna <bagasbanuna02@gmail.com>
2025-12-17 11:40:01 +0800

    Fix API Login dan filter 0 di input nomor
    ### No Issue

--- src/app/api/auth/resend/route.ts
+++ src/app/api/auth/resend/route.ts
@@ -18,21 +18,24 @@ export async function POST(req: Request) {
 
     const msg = `HIPMI%20-%20Kode%20ini%20bersifat%20RAHASIA%20dan%20JANGAN%20DI%20BAGIKAN%20KEPADA%20SIAPAPUN%2C%20termasuk%20anggota%20ataupun%20pengurus%20HIPMI%20lainnya.%5Cn%5Cn%3E%3E%20Kode%20OTP%20anda%3A%20${codeOtp}.`;
 
-    const res = await fetch(
-      `https://wa.wibudev.com/code?nom=${nomor}&text=${msg}`,
-      { cache: "no-cache" }
+       const res = await fetch(
+      `https://cld-dkr-prod-wajs-server.wibudev.com/api/wa/code?nom=${nomor}&text=${msg}`,
+      {
+        cache: "no-cache",
+        headers: {
+          Authorization: `Bearer ${process.env.WA_SERVER_TOKEN}`,
+        },
+      }
     );
 
-    const sendWa = await res.json();
-
-    if (sendWa.status !== "success")
+    if (res.status !== 200)
       return NextResponse.json(
-        {
-          success: false,
-          message: "Nomor Whatsapp Tidak Aktif",
-        },
+        { success: false, message: "Nomor Whatsapp Tidak Aktif" },
         { status: 400 }
       );
+
+    const sendWa = await res.text();
+    console.log("WA Response:", sendWa);
 
     const createOtpId = await prisma.kodeOtp.create({
       data: {
