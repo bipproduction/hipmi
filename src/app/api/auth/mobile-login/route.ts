20d05c1cc7c2a4f4e452a9c91bc10461bfb7c72e
bagasbanuna <bagasbanuna02@gmail.com>
2026-01-22 11:58:37 +0800

    component sender wa to all device ( apps & web )

--- src/app/api/auth/mobile-login/route.ts
+++ src/app/api/auth/mobile-login/route.ts
@@ -1,6 +1,7 @@
 import { prisma } from "@/lib";
 import { randomOTP } from "@/app_modules/auth/fun/rondom_otp";
 import { NextResponse } from "next/server";
+import { sendCodeOtp } from "@/lib/code-otp-sender";
 
 export async function POST(req: Request) {
   try {
@@ -31,30 +32,21 @@ export async function POST(req: Request) {
     if (!createOtpId)
       return NextResponse.json(
         { success: false, message: "Gagal mengirim kode OTP" },
-        { status: 400 }
+        { status: 400 },
       );
 
-    // const msg = `HIPMI - Kode ini bersifat RAHASIA dan JANGAN DI BAGIKAN KEPAADA SIAPAPUN, termasuk anggota ataupun pengurus HIPMI lainnya.\n\n\n> Kode OTP anda: ${codeOtp}.`;
-    // const encodedMsg = encodeURIComponent(msg);
-    const msg = `HIPMI%20-%20Kode%20ini%20bersifat%20RAHASIA%20dan%20JANGAN%20DI%20BAGIKAN%20KEPADA%20SIAPAPUN%2C%20termasuk%20anggota%20ataupun%20pengurus%20HIPMI%20lainnya.%20Kode%20OTP%20anda%3A%20${codeOtp}.`;
+    const resSendCode = await sendCodeOtp({
+      nomor,
+      codeOtp: codeOtp.toString(),
+    });
 
-    const res = await fetch(
-      `https://cld-dkr-prod-wajs-server.wibudev.com/api/wa/code?nom=${nomor}&text=${msg}`,
-      {
-        cache: "no-cache",
-        headers: {
-          Authorization: `Bearer ${process.env.WA_SERVER_TOKEN}`,
-        },
-      }
-    );
-
-    if (res.status !== 200)
+    if (resSendCode.status !== 200)
       return NextResponse.json(
         { success: false, message: "Nomor Whatsapp Tidak Aktif" },
-        { status: 400 }
+        { status: 400 },
       );
 
-    const sendWa = await res.text();
+    const sendWa = await resSendCode.text();
     console.log("WA Response:", sendWa);
 
     return NextResponse.json(
@@ -64,7 +56,7 @@ export async function POST(req: Request) {
         kodeId: createOtpId.id,
         isAcceptTerms: user.termsOfServiceAccepted,
       },
-      { status: 200 }
+      { status: 200 },
     );
   } catch (error) {
     return NextResponse.json(
@@ -73,7 +65,7 @@ export async function POST(req: Request) {
         message: "Terjadi masalah saat login",
         reason: error as Error,
       },
-      { status: 500 }
+      { status: 500 },
     );
   }
 }
